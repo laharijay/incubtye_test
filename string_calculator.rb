1065930a383e8bfc55b3d691ee002aded2d3d177
Jay Lahari <jay.lahari1999@gmail.com>
2024-02-15 17:17:10 +0530

    Handle negative and multiple negative num

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,13 +3,22 @@ class StringCal
     return 0 if str.empty?
     delimiter = remove_delimeters(str)
     str.gsub!(/^\/\/.\n/, "")
-    str.split(/[#{delimiter}]/).map(&:to_i).sum
+    numbers = str.split(/[#{delimiter}]/).map(&:to_i)
+    neg_numbers = check_negative_number(numbers)
+    raise "negative numbers not allowed #{neg_numbers.join(',')}" unless neg_numbers.empty?
+    numbers.sum
+  rescue => e
+    e.message
   end
 
   private
   def remove_delimeters(str)
     delimiter_pattern = str.match(/^\/\/(.)\n/)
     delimiter = delimiter_pattern ? Regexp.escape(delimiter_pattern[1]) : "[,\n]"
+  end
+   
+  def  check_negative_number(numbers)
+    numbers.select{|n| n.negative? }
   end
 
 end
