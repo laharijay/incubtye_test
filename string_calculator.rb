7e5521ec79f235d071c1a26680b3513fec7b6757
Jay Lahari <jay.lahari1999@gmail.com>
2024-02-15 17:10:08 +0530

    Handle different delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,15 @@
 class StringCal
   def add(str)
     return 0 if str.empty?
-    str.gsub!("\n", ",")
-    # numbers =  str.split(",")
-    sum = str.split(",").map(&:to_i).sum
+    delimiter = remove_delimeters(str)
+    str.gsub!(/^\/\/.\n/, "")
+    str.split(/[#{delimiter}]/).map(&:to_i).sum
+  end
+
+  private
+  def remove_delimeters(str)
+    delimiter_pattern = str.match(/^\/\/(.)\n/)
+    delimiter = delimiter_pattern ? Regexp.escape(delimiter_pattern[1]) : "[,\n]"
   end
 
 end
